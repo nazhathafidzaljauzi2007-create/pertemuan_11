f1450187f72db83d0aafc20044880fb83413cf56
nazhat <nazhathafidzaljauzi2007@gmail.com>
2026-05-12 17:32:54 +0700

    membuat library

---
+++ Asosiasi/Asosiasi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class dokter;
+class pasien // asosiasi
+{
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pnama) : nama(pnama)
+    {
+        cout << "Pasien \"" << nama << "\" ada \n";
+    }
+
+    ~pasien()
+    {
+        cout << "Pasien \"" << nama << "\" tidak ada \n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
